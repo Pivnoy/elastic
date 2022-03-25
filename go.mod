4ba1bb1adb7ec9debe98a2ce9260348c09d3e201
Евгений Макарьев <evgenij_makarev20021@mail.ru>
2022-03-25 22:18:11 +0300

    Elastic init

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module elastic
 
 go 1.16
+
+require github.com/elastic/go-elasticsearch/v8 v8.1.0
--- main/main.go
+++ main/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/elastic/go-elasticsearch/v8"
+	_ "github.com/elastic/go-elasticsearch/v8"
+	"io/ioutil"
+	"log"
+)
 
 func main() {
-	fmt.Println("Tri ytra blyat")
+	cert, err := ioutil.ReadFile("/home/evg/http_ca.crt")
+	cfg := elasticsearch.Config{
+		Addresses: []string{
+			"https://localhost:9200",
+		},
+		Username: "elastic",
+		Password: "+Nk3RJlDNpE=tEM89iU5",
+		CACert: cert,
+	}
+	es, err := elasticsearch.NewClient(cfg)
+	if err != nil {
+		log.Fatal("Error")
+	}
+	res, err := es.Info()
+	if err != nil {
+		log.Fatal("Error 1")
+	}
+	fmt.Println(res)
+	if res.IsError() {
+		fmt.Println("Error 2")
+	}
 }
