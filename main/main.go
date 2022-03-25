32a8de7a1c57573861f785d7e2e4c1cff11232c4
Евгений Макарьев <evgenij_makarev20021@mail.ru>
2022-03-25 23:25:02 +0300

    Push

--- main/main.go
+++ main/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/elastic/go-elasticsearch/v8"
 	_ "github.com/elastic/go-elasticsearch/v8"
@@ -12,11 +15,11 @@ func main() {
 	cert, err := ioutil.ReadFile("/home/evg/http_ca.crt")
 	cfg := elasticsearch.Config{
 		Addresses: []string{
-			"https://localhost:9200",
+			"https://localhost:9200/",
 		},
 		Username: "elastic",
 		Password: "+Nk3RJlDNpE=tEM89iU5",
-		CACert: cert,
+		CACert:   cert,
 	}
 	es, err := elasticsearch.NewClient(cfg)
 	if err != nil {
@@ -26,8 +29,50 @@ func main() {
 	if err != nil {
 		log.Fatal("Error 1")
 	}
-	fmt.Println(res)
 	if res.IsError() {
 		fmt.Println("Error 2")
+	}
+	var buffer bytes.Buffer
+	query := map[string]interface{}{
+		"query": map[string]interface{}{
+			"match": map[string]interface{}{
+				"status": "CI PASS",
+			},
+		},
+	}
+	if err := json.NewEncoder(&buffer).Encode(query); err != nil {
+		log.Fatal("Error in buffer request")
+	}
+
+	res, err = es.Search(
+		es.Search.WithContext(context.Background()),
+		es.Search.WithIndex("ci_runs"),
+		es.Search.WithBody(&buffer),
+		es.Search.WithTrackTotalHits(true),
+	)
+	if res.IsError() {
+		log.Fatal("Error in searching")
+	}
+	var e map[string]interface{}
+	if err := json.NewDecoder(res.Body).Decode(&e); err != nil {
+		log.Fatal("Error in parse")
+	}
+	hitsMap := e["hits"].(map[string]interface{})
+	tests := hitsMap["hits"].([]interface{})
+	var results = make([]map[string]interface{}, 10)
+	for _, k := range tests {
+		results = append(results, k.(map[string]interface{}))
+	}
+	var source = make([]map[string]interface{}, 10)
+	for _, k := range results {
+		if k["_source"] != nil {
+			source = append(source, k["_source"].(map[string]interface{}))
+		}
+		//fmt.Println(k["_source"])
+	}
+	for _, l := range source {
+		if l["testlist"] != nil {
+			fmt.Println(l["testlist"])
+		}
 	}
 }
